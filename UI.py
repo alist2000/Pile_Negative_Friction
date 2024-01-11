b16a65f5db5fa6fb647194d50aa866e7e57da7ac
ali <ali.s.tag2000@gmail.com>
2024-01-11 11:33:41 +0330

    Unit added. Qe (End bearing capacity added) Better appearance.

--- UI.py
+++ UI.py
@@ -10,24 +10,49 @@ import copy
 from mainCalc import MainCalc
 from plot import Plot
 from image import ImageWindow
+from Unit import Unit
 
 
-class tabWidget(QWidget):
+class tabWidget(QWidget, Unit):
     def __init__(self):
         super().__init__()
+        # Set the main window size
+        self.setGeometryToCenter(930, 600)
+        self.setWindowTitle("Negative Skin Friction")
 
-        self.setWindowTitle("Pile Negative Friction")
-
+        hLayout1 = QHBoxLayout()
+        self.yesNoLayout = QHBoxLayout()
         hLayout2 = QHBoxLayout()
 
-        diameter = QLabel("Pile Diameter: ")
+        QeLabel = QLabel("Do you want to consider the 'End Bearing Capacity' in the calculations?")
+        self.yes_qe = QRadioButton("Yes")
+        self.no_qe = QRadioButton("No")
+        self.no_qe.setChecked(True)
+        Qe = QLabel(f"Qe ({self.forceUnit}): ")
+        self.QeNum = QDoubleSpinBox()
+        self.QeNum.setRange(0, 10000)
+        if self.no_qe.isChecked():
+            self.QeNum.setEnabled(False)
+
+        hLayout1.addWidget(QeLabel)
+        self.yesNoLayout.addWidget(self.yes_qe)
+        self.yesNoLayout.addWidget(self.no_qe)
+        hLayout1.addLayout(self.yesNoLayout)
+        hLayout1.addWidget(Qe)
+        hLayout1.addWidget(self.QeNum)
+        self.yes_qe.toggled.connect(self.on_button_toggled)
+        self.no_qe.toggled.connect(self.on_button_toggled)
+        diameter = QLabel(f"Pile Diameter ({self.lengthUnit}): ")
         diameterNum = QDoubleSpinBox()
+        diameterNum.setRange(0.1, 10000)
+        diameterNum.setValue(1)
+
         hLayout2.addWidget(diameter)
         hLayout2.addWidget(diameterNum)
-        Qd = QLabel("Permanent load (Qd): ")
+        Qd = QLabel(f"Permanent load (Qd) ({self.forceUnit}): ")
         QdNum = QDoubleSpinBox()
-        QdNum.setRange(0, 10000)
-        QdNum.setValue(100)
+        QdNum.setRange(0.1, 10000)
+        QdNum.setValue(700)
         hLayout2.addWidget(Qd)
         hLayout2.addWidget(QdNum)
 
@@ -41,25 +66,49 @@ class tabWidget(QWidget):
         # Add tabs to widget
         mainLayout = QHBoxLayout()
         layout = QVBoxLayout()
-        soilProp = SoilProperties(diameterNum, QdNum, deltaHNum, mainLayout)
+        soilProp = SoilProperties(self.QeNum, diameterNum, QdNum, deltaHNum, mainLayout)
         layout.addWidget(soilProp)
         runButton = QPushButton("Run")
         runButton.clicked.connect(soilProp.output)
 
+        layout.addLayout(hLayout1)
         layout.addLayout(hLayout2)
 
         layout.addWidget(runButton)
 
-        sign = QLabel("Made by: Ali Safari")
+        sign = QLabel("Made by: Ali Safari                     contact: ali.s.tag2000@gmail.com")
+        sign.setStyleSheet("QLabel { color: rgba(0, 0, 0, 100); }")
+
         layout.addWidget(sign)
         mainLayout.addLayout(layout)
 
         self.setLayout(mainLayout)
 
+    def on_button_toggled(self, checked):
+        if "Yes" in self.sender().text() and checked:
+            self.QeNum.setEnabled(True)
+            self.no_qe.setChecked(False)
+        elif "No" in self.sender().text() and checked:
+            self.QeNum.setEnabled(False)
+            self.yes_qe.setChecked(False)
+            self.QeNum.setValue(0)
 
-class SoilProperties(QWidget):
-    def __init__(self, diameterNum, QdNum, deltaHNum, mainLayout, gridBase="spacing"):
+    def setGeometryToCenter(self, width, height):
+        # Get the available screen geometry
+        screen_geometry = QApplication.primaryScreen().availableGeometry()
+
+        # Calculate the position to center the window
+        x = (screen_geometry.width() - width) // 2
+        y = (screen_geometry.height() - height) // 2
+
+        # Set the main window geometry
+        self.setGeometry(x, y, width, height)
+
+
+class SoilProperties(QWidget, Unit):
+    def __init__(self, Qe, diameterNum, QdNum, deltaHNum, mainLayout, gridBase="spacing"):
         super(SoilProperties, self).__init__()
+        self.Qe = Qe
         self.gridBase = gridBase
         self.diameterNum = diameterNum
         self.QdNum = QdNum
@@ -69,6 +118,7 @@ class SoilProperties(QWidget):
         self.layout = QVBoxLayout(self)
         self.coordinateButton = QRadioButton("Display Soil Layer as Coordinate")
         self.spacingButton = QRadioButton("Display Soil Layer as Spacing")
+        self.spacingButton.setChecked(True)
         self.layoutBase = QHBoxLayout()
         self.layoutBase.addWidget(self.coordinateButton)
         self.layoutBase.addWidget(self.spacingButton)
@@ -124,7 +174,9 @@ class SoilProperties(QWidget):
         Qd = self.QdNum.value()
         d_pile = self.diameterNum.value()
         delta = self.deltaHNum.value()
-        main = MainCalc(gama, h, qf_max, beta_cu, beta_cu_value, d_pile, Qd, delta)
+        Qe = self.Qe.value()
+        print(Qe)
+        main = MainCalc(gama, h, qf_max, beta_cu, beta_cu_value, d_pile, Qd, Qe, delta)
         plot = Plot(main.h_list, main.rn_qs, main.qs_qd)
         label = QLabel()
         pixmap = QPixmap("plot.png")
@@ -149,7 +201,7 @@ class SoilProperties(QWidget):
         return sortedGrid
 
 
-class SoilPropDefine(QWidget):
+class SoilPropDefine(QWidget, Unit):
     def __init__(self, coordinateButton, spacingButton):
         super(SoilPropDefine, self).__init__()
         self.coordinateButton = coordinateButton
@@ -173,11 +225,11 @@ class SoilPropDefine(QWidget):
         self.buttons_layout.addWidget(self.buttonDelete)
         self.tableWidgetXGrid = QTableWidget(0, 5)
         self.tableWidgetXGrid.insertRow(0)
-        nameLabel = QLabel("\u03B3")
-        coordLabel = QLabel(f"H ({self.grid_base.capitalize()})")
+        nameLabel = QLabel(f"\u03B3 ({self.densityUnit})")
+        coordLabel = QLabel(f"H ({self.grid_base.capitalize()}) ({self.lengthUnit})")
         beta_or_cu = QLabel("\u03B2 or Cu")
-        beta_or_cu_num = QLabel("\u03B2 or Cu value")
-        qf_max = QLabel("Qf max")
+        beta_or_cu_num = QLabel(f"\u03B2 or Cu value (None or {self.pressureUnit})")
+        qf_max = QLabel(f"Qf max ({self.pressureUnit})")
         nameLabel.setAlignment(Qt.AlignCenter)
         coordLabel.setAlignment(Qt.AlignCenter)
         beta_or_cu.setAlignment(Qt.AlignCenter)
@@ -188,6 +240,11 @@ class SoilPropDefine(QWidget):
         self.tableWidgetXGrid.setCellWidget(0, 2, beta_or_cu)
         self.tableWidgetXGrid.setCellWidget(0, 3, beta_or_cu_num)
         self.tableWidgetXGrid.setCellWidget(0, 4, qf_max)
+        # Set cell size
+        for row in range(1):
+            self.tableWidgetXGrid.setRowHeight(row, 50)  # Set row height
+            for col in range(5):
+                self.tableWidgetXGrid.setColumnWidth(col, 155)  # Set column width
         self.layoutTable = QHBoxLayout()
         self.layoutTable.addWidget(self.tableWidgetXGrid)
         self.layoutTable.addLayout(self.buttons_layout)
@@ -204,7 +261,7 @@ class SoilPropDefine(QWidget):
 
     def on_button_toggled(self, checked):
         if "Coordinate" in self.sender().text() and checked:
-            item = QLabel("H (Coordinate")
+            item = QLabel(f"H (Coordinate) {self.lengthUnit}")
             item.setAlignment(Qt.AlignCenter)
             self.tableWidgetXGrid.setCellWidget(0, 1, item)
             self.grid_base = "coordinate"
@@ -226,7 +283,7 @@ class SoilPropDefine(QWidget):
 
         elif "Spacing" in self.sender().text() and checked:
             self.grid_base = "spacing"
-            item = QLabel("H (Spacing)")
+            item = QLabel(f"H (Spacing) {self.lengthUnit}")
             item.setAlignment(Qt.AlignCenter)
             self.tableWidgetXGrid.setCellWidget(0, 1, item)
             firstCoord = 0
@@ -261,6 +318,15 @@ class SoilPropDefine(QWidget):
         spinBoxH.setAlignment(Qt.AlignCenter)
         spinBoxBetaCu.setAlignment(Qt.AlignCenter)
         spinBoxBetaQfMax.setAlignment(Qt.AlignCenter)
+
+        spinBoxGamma.setValue(20)
+
+        spinBoxH.setValue(10)
+
+        spinBoxBetaCu.setValue(0.5)
+
+        spinBoxBetaQfMax.setValue(81)
+
         self.tableWidgetXGrid.setCellWidget(row, 0, spinBoxGamma)
         self.tableWidgetXGrid.setCellWidget(row, 1, spinBoxH)
         self.tableWidgetXGrid.setCellWidget(row, 2, betaCu)
