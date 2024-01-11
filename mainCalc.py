f1b9cfd516ff40ead020c4739b0e219b39573bee
ali <ali.s.tag2000@gmail.com>
2024-01-11 11:32:49 +0330

    consider Qe (End bearing capacity.)

--- mainCalc.py
+++ mainCalc.py
@@ -9,7 +9,7 @@ import numpy as np
 
 
 class MainCalc:
-    def __init__(self, gama, h, max_qf_list, beta_or_cu, beta_or_cu_num, d_pile, Qd, delta=0.1):
+    def __init__(self, gama, h, max_qf_list, beta_or_cu, beta_or_cu_num, d_pile, Qd, Qe=0, delta=0.1):
         self.gama = gama
         self.h = h
         self.max_qf_list = max_qf_list
@@ -17,6 +17,7 @@ class MainCalc:
         self.beta_or_cu_num = beta_or_cu_num
         self.d_pile = d_pile
         self.Qd = Qd
+        self.Qe = Qe
         self.delta = delta
         effectivePressure = EffectivePressure(self.gama, self.h)
         h_total = sum(h)
@@ -39,14 +40,13 @@ class MainCalc:
             Qs_list.append(qs.Qs)
             self.h_list.append(i)
 
-        Rn = Qs_list[-1]
+        RnNum = Rn(Qs_list[-1], self.Qe).Rn
         self.rn_qs = []
         self.qs_qd = []
 
         for qsItem in Qs_list:
-            self.rn_qs.append(Rn - qsItem)
+            self.rn_qs.append(RnNum - qsItem)
             self.qs_qd.append(qsItem + self.Qd)
-
 
 # main = MainCalc([19, 10, 10], [8, 10, 4], [67, 0, 81], ["beta", "cu", "beta"], [0.29, 70, 0.37], 1, 700, 1)
 # print(main.rn_qs)
